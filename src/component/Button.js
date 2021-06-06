969413729ad244f8d72551e7bf4a88f908531a15
hmusman@devsbeta.com <hmusman@devsbeta.com>
2021-06-06 21:54:10 +0500

    components and props
    add proptypes
    add default props
    add components

---
+++ src/component/Button.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ( {text,color} ) => {
+    
+    return (
+        <button style={{ backgroundColor: color }} className="btn"> {text} </button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+} 
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string
+}
+
+export default Button
