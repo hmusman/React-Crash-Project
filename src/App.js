64aed07f28a80088745b708ea8e5eeb44f4246f3
hmusman@devsbeta.com <hmusman@devsbeta.com>
2021-06-07 12:00:39 +0500

    tasks and map function
    1) add tasks component
    2) add map with tasks array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Header from './component/Header'
+import Tasks from './component/Tasks'
+
 export class App extends Component {
   render() {
     return (
       <div className="container">
         <Header />
+        <Tasks />
       </div>
     )
   }
