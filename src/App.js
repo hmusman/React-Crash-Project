f7574d2d35a866b431926aaa374bf1471700f59c
hmusman@devsbeta.com <hmusman@devsbeta.com>
2021-06-07 12:51:10 +0500

    add global state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 import Header from './component/Header'
 import Tasks from './component/Tasks'
+import {useState} from 'react'
 
-export class App extends Component {
-  render() {
-    return (
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+        
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }     
+
+])
+    
+  return (
       <div className="container">
-        <Header />
-        <Tasks />
-      </div>
-    )
-  }
+      <Header />
+      <Tasks tasks={ tasks }/>
+    </div>
+  )
 }
 
 export default App
